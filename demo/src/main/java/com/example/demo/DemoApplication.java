754ae6c528aa10078db36b7e840782e3094e02ef
DKS0483411 <DKS0483411@FC-W764-EC-058.dcsg.com>
2018-10-25 13:35:37 -0400

    Added Logging

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,9 +14,24 @@ import com.example.demo.config.AppConfig;
 @SpringBootApplication 
 public class DemoApplication  {
 
+	private static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+	
 	public static void main(String[] args) {
+		
+		
+		log.info("Starting Demo Application ");
 		Class[] configClasses = {DemoApplication.class, AppConfig.class};
+		ApplicationContext ctx = SpringApplication.run(configClasses, args);
 		SpringApplication.run(configClasses, args);
+
+		log.info("Initialized config classes, JAX-RS server endpoint & resources as Spring Beans");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            log.info(beanName);
+        }
+
 	}
 	
 	
--- demo/src/main/java/com/example/demo/service/HelloServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/HelloServiceImpl.java
@@ -1,13 +1,16 @@
 package com.example.demo.service;
 
 import javax.ws.rs.Path;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloServiceImpl implements HelloService{
 
+	private static Logger log = LoggerFactory.getLogger(HelloServiceImpl.class);
 	@Override
 	public String sayHello(String name) {
-		return "Hello " + name + ". It's a beautiful day!";
+		log.debug("Hello " + name + "! Greetings from Pivotal.");
+		return "Hello " + name + "! Greetings from Pivotal.";
 	}
 
 }
